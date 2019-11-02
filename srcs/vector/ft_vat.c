c6cd87d846a6d3e752d851330198657da6643c35
Ahugh <ahugh@student.21-school.ru>
2019-11-02 13:29:55 +0300

    fixed shift in func ft_vat

--- srcs/vector/ft_vat.c
+++ srcs/vector/ft_vat.c
@@ -6,7 +6,7 @@
 /*   By: ahugh <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/24 21:50:01 by ahugh             #+#    #+#             */
-/*   Updated: 2019/02/26 14:35:02 by ahugh            ###   ########.fr       */
+/*   Updated: 2019/11/02 13:18:32 by ahugh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,13 @@
 
 void		*ft_vat(t_vector *v, size_t position)
 {
+	size_t	shift;
+
 	if (v)
-		return ((void*)(&v->const_con)[position]);
+	{
+		shift = v->type_size * position;
+		if (shift < ft_vsize(v))
+			return ((void*)(v->const_con + shift));
+	}
 	return (0);
 }
