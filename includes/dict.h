5d2072f4cb613a5e11b0264782834f98c8968854
Ahugh <ahugh@student.21-school.ru>
2019-11-05 23:43:22 +0300

    save state

--- includes/dict.h
+++ includes/dict.h
@@ -6,7 +6,7 @@
 /*   By: ahugh <ahugh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 13:36:05 by ahugh             #+#    #+#             */
-/*   Updated: 2019/11/05 20:38:35 by ahugh            ###   ########.fr       */
+/*   Updated: 2019/11/05 23:27:21 by ahugh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,27 @@
 # define GROW_RATE(d) ((d)->mask * 3)
 # define PERTURB_SHIFT 5
 
+# define DKIX_ACTIVE(ix) ((ix) & 0x80000000)
+# define DKIX_DUMMY(ix)  ((ix) & 0x40000000)
+# define DKIX_ERROR(ix)  ((ix) & 0x20000000)
+# define DKIX_EMPTY(ix)  ((ix) == 0)
+# define DKIX(ix)        ((ix) & 0x0FFFFFFF)
+
+/*
+** ix bit field which containing values indices slots and his flags
+** ix & 0x0FFFFFFF containing indices
+** ix & 0x80000000 active slot
+** ix & 0x40000000 dummy slot
+** ix & 0x20000000 error slot
+** ix & 0x10000000 empty slot
+*/
+
 typedef struct		s_slot
 {
-	unsigned long	ix;
 	unsigned long	hash;
 	char			*key;
 	void			*value;
+	unsigned int	ix;
 }					t_slot;
 
 t_slot				**ft_slotsnew(long length);
@@ -30,17 +45,17 @@ void				ft_slotsdel(t_slot ***slots, void del(void*), long length);
 
 typedef struct		s_dict
 {
-	long			fill;
-	long			used;
-	long			mask;
 	t_slot			**table;
 	t_vector		*keys;
 	t_vector		*items;
+	unsigned long	mask;
+	unsigned int	used;
+	unsigned int	fill;
 }					t_dict;
 
 t_dict				*ft_dictnew(void);
 void				ft_dictdel(t_dict **dict, void del(void*));
-int					ft_dictset(char *key, void *value);
+int					ft_dictset(t_dict *dict, char *key, void *value);
 
 unsigned long		ft_hash(char *key);
 
--- srcs/dictionary/ft_dictset.c
+++ srcs/dictionary/ft_dictset.c
@@ -6,15 +6,33 @@
 /*   By: ahugh <ahugh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 20:34:10 by ahugh             #+#    #+#             */
-/*   Updated: 2019/11/05 20:38:35 by ahugh            ###   ########.fr       */
+/*   Updated: 2019/11/05 23:29:02 by ahugh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int					ft_dictset(char *key, void *value)
+int					ft_dictset(t_dict *dict, char *key, void *value)
 {
-	if (key == NULL || value == NULL)
+	unsigned long	hash;
+	unsigned long	perturb;
+	unsigned long	i;
+
+	if (dict == NULL || key == NULL || value == NULL)
 		return (FALSE);
+	hash = ft_hash(key);
+	perturb = hash;
+	i = hash & dict->mask;
+	while (TRUE)
+	{
+		if (DKIX_DUMMY(dict->table[i]->ix) || DKIX_EMPTY(dict->table[i]->ix))
+		{
+
+		}
+		if (DKIX_ERROR(dict->table[i]->ix))
+			return (FALSE);
+		perturb >>= PERTURB_SHIFT;
+		i = (i*5 + perturb + 1) & mask;
+	}
 	return (TRUE);
 }
