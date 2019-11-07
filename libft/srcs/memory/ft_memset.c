76095a598a7bca839a91aa4dfd7e89b1343d5a7f
Ahugh <ahugh@student.21-school.ru>
2019-11-07 17:49:18 +0300

    accelerated memset

--- libft/srcs/memory/ft_memset.c
+++ libft/srcs/memory/ft_memset.c
@@ -6,18 +6,30 @@
 /*   By: ahugh <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/21 11:04:41 by ahugh             #+#    #+#             */
-/*   Updated: 2019/01/21 21:34:15 by ahugh            ###   ########.fr       */
+/*   Updated: 2019/11/07 17:43:03 by ahugh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *b, int c, size_t len)
+void					*ft_memset(void *b, int c, size_t len)
 {
-	unsigned char	*pb;
+	uint64_t			*big;
+	uint64_t			w;
+	uint8_t				*small;
+	uint8_t				s;
 
-	pb = (unsigned char*)b;
+	big = (uint64_t*)b;
+	w = c;
+	w |= w << 8 | w << 16 | w << 24 | w << 32 | w << 40 | w << 48 | w << 56;
+	while (len > 7ULL)
+	{
+		*big++ = w;
+		len -= 8ULL;
+	}
+	s = c;
+	small = (uint8_t*)big;
 	while (len--)
-		*pb++ = (unsigned char)c;
+		*small++ = s;
 	return (b);
 }
