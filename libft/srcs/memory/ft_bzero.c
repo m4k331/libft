5df8a69628b6ec7887489dfd8d2e481d4da24999
Ahugh <ahugh@student.21-school.ru>
2019-11-07 07:24:26 +0300

    fixed bzero

--- libft/srcs/memory/ft_bzero.c
+++ libft/srcs/memory/ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: ahugh <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/21 12:26:46 by ahugh             #+#    #+#             */
-/*   Updated: 2019/11/07 06:19:56 by ahugh            ###   ########.fr       */
+/*   Updated: 2019/11/07 06:25:57 by ahugh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,16 @@
 
 void			ft_bzero(void *s, size_t n)
 {
-	size_t		count_big;
-	size_t		count_small;
 	uint64_t	*big;
 	uint8_t		*small;
 
-	count_big = n >> 3ULL;
-	count_small = n - (count_big << 3ULL);
-	small = s;
-	while (count_small--)
-		*small++ = 0;
-	small--;
-	big = (uint64_t*)small;
-	while (count_big--)
+	big = (uint64_t*)s;
+	while (n > 7ULL)
+	{
 		*big++ = 0ULL;
+		n -= 8ULL;
+	}
+	small = (uint8_t*)big;
+	while (n--)
+		*small++ = 0;
 }
