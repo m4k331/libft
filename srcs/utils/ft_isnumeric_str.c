dfdefaecafc6521e66771a7b764286983de133f8
m4k331 <m4k331@github.com>
2019-11-23 00:24:48 +0300

    add condition in ft_isnumeric_str if string is empty function return FALSE

--- srcs/utils/ft_isnumeric_str.c
+++ srcs/utils/ft_isnumeric_str.c
@@ -6,7 +6,7 @@
 /*   By: ahugh <ahugh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/19 21:12:56 by ahugh             #+#    #+#             */
-/*   Updated: 2019/11/19 21:15:30 by ahugh            ###   ########.fr       */
+/*   Updated: 2019/11/23 00:22:50 by ahugh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,17 @@
 int			ft_isnumeric_str(const char *str, int is_spaces)
 {
 	char	*s;
+	int8_t	empty;
 
 	s = (char*)str;
 	if (is_spaces == TRUE)
 		while (ft_isspace(*s))
 			s++;
+	empty = ft_isdigit(*s) ? FALSE : TRUE;
 	while (ft_isdigit(*s))
 		s++;
 	if (is_spaces == TRUE)
 		while (ft_isspace(*s))
 			s++;
-	return (*s == '\0');
+	return (*s == '\0' && !empty);
 }
